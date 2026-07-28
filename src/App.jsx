fd1f1ef8c6217c4de47f7e157631b72eece81fa5
timurci <timurcakmakoglu@gmail.com>
2026-07-28 22:54:14 +0300

    chore: initial files

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <h1 className="text-4xl font-bold text-blue-600 p-10">
+      Tailwind works! 🎉
+    </h1>
+  )
+}
+
+export default App
